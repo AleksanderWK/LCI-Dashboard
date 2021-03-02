6b5b48b52b83f725b384dc38c874a15e9ef7079e
Eirik Schøien <eirischo@stud.ntnu.no>
2021-03-02 01:29:41 +0100

    #14 Adds stop recording button and toggle

--- frontend/app/src/components/dashboard/RecordingButton.tsx
+++ frontend/app/src/components/dashboard/RecordingButton.tsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "@material-ui/core/Button";
-//import {makeStyles, Theme, createStyles, Icon} from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-
-/*const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        button: {}
-    })
-);
-*/
+import StopIcon from "@material-ui/icons/Stop";
 
 export default function RecordingButton(): JSX.Element {
-    //const classes = useStyles();
+    const [isRecording, setIsRecording] = useState<boolean>(false);
+
+    function handleStartRecordingClick(): void {
+        //ADD START SIGNAL
+        setIsRecording(true);
+    }
+
+    function handleStopRecordingClick(): void {
+        //ADD STOP SIGNAL
+        setIsRecording(false);
+    }
 
     return (
-        <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />}>
-            Start recording
-        </Button>
+        <div>
+            {!isRecording ? (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<PlayArrowIcon />}
+                    onClick={() => {
+                        handleStartRecordingClick();
+                    }}
+                >
+                    Start recording
+                </Button>
+            ) : (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<StopIcon />}
+                    onClick={() => {
+                        handleStopRecordingClick();
+                    }}
+                >
+                    Stop recording
+                </Button>
+            )}
+        </div>
     );
 }
