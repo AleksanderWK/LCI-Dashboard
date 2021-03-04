911592f2e342f61d19a8a1a05f9f9b54883cd8a7
aleksawk <aleksawk@stud.ntnu.no>
2021-03-04 09:54:22 +0100

    #32 Fix pipeline test error

--- frontend/app/src/components/createsessionview/__tests__/CreateSessionView.test.tsx
+++ frontend/app/src/components/createsessionview/__tests__/CreateSessionView.test.tsx
@@ -4,13 +4,23 @@ import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreateSessionView from "../../../pages/CreateSessionView";
 import CreateSession from "../CreateSession";
+import {RecoilRoot} from "recoil";
 
 it("create session view matches snapshot", () => {
-    const {baseElement} = render(<CreateSessionView />);
+    const {baseElement} = render(
+        <RecoilRoot>
+            <CreateSessionView />
+        </RecoilRoot>
+    );
     expect(baseElement).toMatchSnapshot();
 });
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<CreateSessionView />, div);
+    ReactDOM.render(
+        <RecoilRoot>
+            <CreateSessionView />
+        </RecoilRoot>,
+        div
+    );
 });
