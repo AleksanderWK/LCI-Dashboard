e5635075286f5942d6017a740114594060845950
ErlendMariusOmmundsen <emommundsen@gmail.com>
2021-03-08 11:20:35 +0100

    #9 add start view test

---
+++ frontend/app/src/components/startview/__tests__/StartView.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {render} from "@testing-library/react";
+import StartView from "./../../../pages/StartView";
+
+
+// Snapshot test does not work due to dynamic MUI id's in the MUI table
+// it("creating start view matches snapshot", () => {
+//     const {baseElement} = render(<StartView />);
+//     expect(baseElement).toMatchSnapshot();
+// });
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<StartView />, div);
+});
