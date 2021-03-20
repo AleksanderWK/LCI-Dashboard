152f363f888ffbf9198227d774ce353196e245a4
Runar Sæther <runarsather@hotmail.com>
2021-03-20 17:50:35 +0100

    #50 Compile recording data to a single object per session

--- frontend/src/db/recordings.js
+++ frontend/src/db/recordings.js
@@ -4,38 +4,44 @@ db.recordings.persistence.setAutocompactionInterval(10000);
 
 function pushDataPointToSession(timestamp, data, sessionId, recordingId) {
   db.recordings.update(
-    { sessionId: sessionId, recordingId: recordingId },
-    /* SUGGESTION 1:
-      {
-        $push: {
-          "data.cl": [timestamp, data.cl],
-          "data.pd": [timestamp, data.pd],
-          "data.fam": [timestamp, data.fam],
-          "data.ipd": [timestamp, data.ipd],
-          "data.pa": [timestamp, data.pa],
-          "data.eng": [timestamp, data.eng],
-          "data.ps": [timestamp, data.ps],
-          "data.er": [timestamp, data.er],
-          "data.ms": [timestamp, data.ms],
-          "data.esf": [timestamp, data.esf],
-        },
-      },
-      */
-
-    // SUGGESTION 2:
+    { sessionId: sessionId },
     {
+      $min: {
+        startTime: timestamp,
+      },
+      $max: {
+        endTime: timestamp,
+      },
       $push: {
-        "data.timestamps": timestamp,
-        "data.cl": data.cl,
-        "data.pd": data.pd,
-        "data.fam": data.fam,
-        "data.ipd": data.ipd,
-        "data.pa": data.pa,
-        "data.eng": data.eng,
-        "data.ps": data.ps,
-        "data.er": data.er,
-        "data.ms": data.ms,
-        "data.esf": data.esf,
+        // One array for each variable and one common timestamp array
+        ["data." + recordingId + ".timestamps"]: timestamp,
+        ["data." + recordingId + ".cl"]: data.cl,
+        ["data." + recordingId + ".pd"]: data.pd,
+        ["data." + recordingId + ".fam"]: data.fam,
+        ["data." + recordingId + ".ipd"]: data.ipd,
+        ["data." + recordingId + ".pa"]: data.pa,
+        ["data." + recordingId + ".eng"]: data.eng,
+        ["data." + recordingId + ".ps"]: data.ps,
+        ["data." + recordingId + ".er"]: data.er,
+        ["data." + recordingId + ".ms"]: data.ms,
+        ["data." + recordingId + ".esf"]: data.esf,
+
+        // ALTERNATIVE: One object per data point containing all variables and timestamp
+        // ["data." + recordingId]: { ...data, timestamp: timestamp },
+
+        // ALTERNATIVE: Timestamp stored for each variable data point
+        /*
+        "data.cl": [timestamp, data.cl],
+        "data.pd": [timestamp, data.pd],
+        "data.fam": [timestamp, data.fam],
+        "data.ipd": [timestamp, data.ipd],
+        "data.pa": [timestamp, data.pa],
+        "data.eng": [timestamp, data.eng],
+        "data.ps": [timestamp, data.ps],
+        "data.er": [timestamp, data.er],
+        "data.ms": [timestamp, data.ms],
+        "data.esf": [timestamp, data.esf],
+        */
       },
     },
     { multi: false, upsert: true },
