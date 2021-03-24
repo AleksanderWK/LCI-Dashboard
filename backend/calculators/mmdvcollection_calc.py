090ceef69ccb6001b072cf90005c5eed2faa899a
Erlend Marius Ommundsen <emommundsen@gmail.com>
2021-03-24 15:47:17 +0100

    #43 add engamement calculator

--- backend/calculators/mmdvcollection_calc.py
+++ backend/calculators/mmdvcollection_calc.py
@@ -1,6 +1,7 @@
 from datastreams import Datastreams
 from datamodels.mmdvcollection import MMDVCollection
 from calculators.pdcalc import PerceivedDifficultyCalculator
+from calculators.engcalc import EngagementCalculator
 
 
 class MMDVCollectionCalculator:
@@ -16,11 +17,14 @@ class MMDVCollectionCalculator:
     def __init__(self, ds: Datastreams):
         self.ds = ds
         self.pd_calc = PerceivedDifficultyCalculator()
+        self.eng_calc = EngagementCalculator()
 
     def calc_pd(self):
         return self.pd_calc.calculate_dataset(self.ds.get_current_eye_tracking_data())
 
     def calculate_all(self):
         result = MMDVCollection()
+        eda_data = self.ds.get_current_eda_data()
         result.pd = self.calc_pd()
+        result.eng = self.eng_calc.calculate_dataset(eda_data)
         return result
