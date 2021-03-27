c460de7e9c69b7aeaba10fadf27674a70ba7c930
Erlend Marius Ommundsen <erlenmom@stud.ntnu.no>
2021-03-27 10:59:36 +0000

    #42 Remove prints, unused imports and result division

--- backend/calculators/engcalc.py
+++ backend/calculators/engcalc.py
@@ -1,10 +1,7 @@
 import neurokit2 as nk
 import numpy as np
-from datamodels.wristband import EDADataPoint
 from calculators.mmdvcalc import MMDVCalculator
 import pandas as pd
-from typing import List
-
 
 class EngagementCalculator(MMDVCalculator):
     dataList = []
@@ -21,17 +18,15 @@ class EngagementCalculator(MMDVCalculator):
         for i in data:
             self.dataList.append(i.value)
 
-        print(self.dataList)
         if len(self.dataList) >= 16:
             cleanEDA = nk.eda_clean(self.dataList[:16])
             phasicEDA = nk.eda_phasic(cleanEDA, sampling_rate=8)
             tonicEDA = sorted(phasicEDA.get("EDA_Tonic").values)
             area = np.trapz(y=tonicEDA, dx=0.125)
-            print(area)
             for i in range(len(self.dataList.copy())):
                 if len(self.dataList) == 16:
                     break
                 self.dataList.pop(0)
-            return area/100
+            return area
 
         return -1
