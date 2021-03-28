e4e7b1451956299795d622a2ea9d831192d9ca86
Eirik Schøien <eirischo@stud.ntnu.no>
2021-03-28 01:19:39 +0100

    #53 Fix rendering and no charts message

--- frontend/app/src/components/dashboard/live/Dashboard.tsx
+++ frontend/app/src/components/dashboard/live/Dashboard.tsx
@@ -1,5 +1,5 @@
-import {createStyles, makeStyles} from "@material-ui/core";
-import {useRecoilState} from "recoil";
+import {createStyles, makeStyles, Typography} from "@material-ui/core";
+import {useRecoilState, useRecoilValue} from "recoil";
 import {Variable} from "../../../constants";
 import {selectedSessionActiveContainersState} from "../../../state/session";
 import Container from "./Container";
@@ -19,44 +19,18 @@ const useStyles = makeStyles(() =>
 function Dashboard(): JSX.Element {
     const classes = useStyles();
 
-    const [activeContainers, setActiveContainers] = useRecoilState(selectedSessionActiveContainersState);
+    const activeContainers = useRecoilValue(selectedSessionActiveContainersState);
 
     return (
         <div className={classes.dashboard}>
-            {/*Object.entries(activeContainers).map(([key, value]) => {
-                if (value.active) {
-                    return <Container variable={Variable[key as keyof typeof Variable]} display={value.display} />;
-                }
-            })*/}
-            {activeContainers.cl.active && (
-                <Container variable={Variable.CognitiveLoad} display={activeContainers.cl.display} />
-            )}
-            {activeContainers.pd.active && (
-                <Container variable={Variable.PerceivedDifficulty} display={activeContainers.pd.display} />
-            )}
-            {activeContainers.fam.active && (
-                <Container variable={Variable.Familiarity} display={activeContainers.fam.display} />
-            )}
-            {activeContainers.ipi.active && (
-                <Container variable={Variable.InformationProcessingIndex} display={activeContainers.ipi.display} />
-            )}
-            {activeContainers.pa.active && (
-                <Container variable={Variable.PhysiologicalArousal} display={activeContainers.pa.display} />
-            )}
-            {activeContainers.eng.active && (
-                <Container variable={Variable.Engagement} display={activeContainers.eng.display} />
-            )}
-            {activeContainers.ps.active && (
-                <Container variable={Variable.PhysiologicalStress} display={activeContainers.ps.display} />
-            )}
-            {activeContainers.er.active && (
-                <Container variable={Variable.EmotionalRegulation} display={activeContainers.er.display} />
-            )}
-            {activeContainers.ms.active && (
-                <Container variable={Variable.MotionStability} display={activeContainers.ms.display} />
-            )}
-            {activeContainers.esf.active && (
-                <Container variable={Variable.EnergySpentFatigue} display={activeContainers.esf.display} />
+            {Object.values(Variable).every((variable) => activeContainers[variable].active === false) ? (
+                <Typography>No charts added</Typography>
+            ) : (
+                Object.values(Variable).map((key) => {
+                    if (activeContainers[key].active) {
+                        return <Container variable={key} display={activeContainers[key].display} />;
+                    }
+                })
             )}
         </div>
     );
